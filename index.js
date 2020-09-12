557ab359a01d04635b6208ca046596541f2d9900
michel alexandrine <michelalexandrine@jetstreet-pro-xx-1.home>
2020-09-12 21:47:10 +0200

    started react config

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function Main() {
+    return (
+        <div>
+            <h3>Welcome to my bull</h3>
+        </div>
+    );
+}
+
+render(<Main />, document.getElementById('app'));
